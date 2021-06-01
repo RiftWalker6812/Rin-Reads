f9400d42e8527f53cd602003a3323d3adcc22761
RiftWalker6812 <45465917+RiftWalker6812@users.noreply.github.com>
2021-05-31 21:47:44 -0400

    V 0.1.1

---
+++ Rin Reads/wwwroot/CodeLib/MangaDexAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rin_Reads.wwwroot.CodeLib
+{
+    public class MangaDexAPI
+    {
+        
+    }
+}
